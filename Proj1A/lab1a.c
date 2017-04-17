95c0dc642aa61980712716b5ed3842e5e455f62d
Hongyang Li <l3848378@outlook.com>
2017-04-17 15:17:00 -0700

    Useless

--- Proj1A/lab1a.c
+++ Proj1A/lab1a.c
@@ -9,11 +9,13 @@
 #include <getopt.h>
 #include <errno.h>
 #include <string.h>
+#include <poll.h>
+#include <signal.h>
 
-
+/*
 void read_buf(int, char *);
 void write_buf(int, char *, int);
-
+*/
 struct termios saved_attributes;
 
 void reset_input_mode (void) {
@@ -43,7 +45,7 @@ void set_input_mode (void) {
   tattr.c_lflag = ISTRIP;
   tattr.c_oflag = 0;
   tattr.c_lflag = 0;
-  tcsetattr (STDIN_FILENO, TCSAFLUSH, &tattr);
+  tcsetattr (STDIN_FILENO, TCSANOW, &tattr);
 }
 
 int main(int argc, char *argv[]) {
@@ -52,9 +54,9 @@ int main(int argc, char *argv[]) {
     {0, 0, 0, 0}
   };
   int shell_flag = 0;
-  int i;
-  while ((i = getopt_long(argc, argv, "", args, NULL)) != -1) {
-    if (i == 's') {
+  int arg_get;
+  while ((arg_get = getopt_long(argc, argv, "", args, NULL)) != -1) {
+    if (arg_get == 's') {
       shell_flag = 1;
     } else {
       printf("Please enter correct commands as shown below!\n");
@@ -62,59 +64,136 @@ int main(int argc, char *argv[]) {
       exit(1);
     }
   }
-  
+
+  set_input_mode();
+
   if (!shell_flag) {
-    char buf[1];
-    read_buf(0, buf, &old_attributes);
-    while (buf[0] != 0x04) {
-      if ((buf[0] == 0x0D)||(buf[0] == 0x0A)) {
-        write_buf(1, "\r\n", 2, &old_attributes);
-      } else { 
-        write_buf(1, buf, 1, &old_attributes);
+    char buf[1024];
+    int read_count = 0;
+    int i = 0;
+    while (1) {
+      read_count = read(0, buf, 1024);
+      if (read_count == -1) {
+        perror("Can not read from stdin");
+        exit(1);
+      } else {
+        for (i = 0; i < read_count; ++i) {
+          switch (buf[i]) {
+            case 0x0D:
+            case 0x0A:
+              write(1, "\r\n", 2);
+              break;
+            case 0x04:
+              exit(0);
+            default:
+              write(1, buf + i, 1);
+          }
+        }
       }
-      read_buf(0, buf, &old_attributes);
     }
+   
   } else {
     int pfd1[2], pfd2[2];
     if (pipe(pfd1) < 0) {
       perror("Creating pipe failed");
-      set_attr(0, &old_attributes);
       exit(1);
     }
     if (pipe(pfd2) < 0) {
       perror("Creating pipe failed");
-      set_attr(0, &old_attributes);
       exit(1);
     }
     int rc = fork();
     if (rc < 0) { // fork failed; exit
       perror("Fork failed\n");
-      set_attr(0, &old_attributes);
       exit(1);
     } else {
       if (rc == 0) { // child (new process)
-        dup2(0, pfd1[0]);
-	dup2(1, pfd2[1]);
+        close(pfd1[1]);
+        close(pfd2[0]);
+        dup2(pfd1[0], 0);
+	      dup2(pfd2[1], 1);
+        close(pfd1[0]);
+        close(pfd2[1]);
         char *child_args[2];
-	child_args[0] = strdup("/bin/bash");
-	child_args[1] = NULL;
-	execvp(child_args[0], child_args);
+	      child_args[0] = strdup("/bin/bash");
+	      child_args[1] = NULL;
+	      if (execvp(child_args[0], child_args) == -1) {
+          perror("execvp() failed!\n");
+          exit(1);
+        }
       } else { // parent goes down this path (main)
-	printf("%d %d\n\r", pfd1[0], pfd1[1]);
-	printf("%d %d\n\r", pfd2[0], pfd2[1]);
+        close(pfd1[0]);
+        close(pfd2[1]);
+        char buf[1024];
+        struct pollfd fds[2];
+        int ret_poll;
+        int i = 0;
+        int j = 0;
+        int read_count = 0;
+        fds[0].fd = 0;
+        fds[1].fd = pfd2[0];
+        fds[0].events = POLLIN | POLLHUP | POLLERR;
+        fds[1].events = POLLIN | POLLHUP | POLLERR;
+        while (1) {
+          ret_poll = poll(fds, 2, 0);
+          if (ret_poll == -1) {
+            perror("poll() failed\n");
+            exit(1);
+          } else {
+            if (ret_poll > 0) {
+              for (i = 0; i < 2; ++i) {
+                if (fds[i].revents & POLLIN) {
+                  read_count = read(fds[i].fd, buf, 1024);
+                  if (read_count == -1) {
+                    perror("read() failed\n");
+                    exit(1);
+                  } else {
+                    for (j = 0; j < read_count; ++j) {
+                      switch (buf[j]) {
+                        case 0x0D:
+                          if (i == 0) {
+                            write(1, "\r\n", 2);
+                            write(pfd1[1], "\n", 1);
+                          } else {
+                            write(1, "\r\n", 1);
+                          }
+                          break;
+                        case 0x0A:
+                          if (i == 0) {
+                            write(1, "\r\n", 2);
+                            write(pfd1[1], "\n", 1);
+                          } else {
+                            write(1, buf + i, 2);
+                          }
+                          break;
+                        case 0x03:
+                          kill(rc, SIGINT);
+                          exit(0);
+                        default:
+                          write(1, buf + i, 1);
+                          if (i == 0) {
+                            write(pfd1[1], buf + i, 1);
+                          }
+                      }
+                    }
+                  }
+                }
+                //if (fds[i].revents & POLLIN) {
+              }
+            }
+          }
+        }
+	      // printf("%d %d\n\r", pfd1[0], pfd1[1]);
+	      // printf("%d %d\n\r", pfd2[0], pfd2[1]);
       }
     }
   }
     
-  exit(0);
   return 0;
 }
-
+/*
 void read_buf(int fd, char *buf) {
-  if (read(fd, buf, 1) == -1) {
-    perror("Can not read from stdin");
-    exit(1);
-  }
+  
 }
 
 void write_buf(int fd, char *buf, int i) {
@@ -122,4 +201,4 @@ void write_buf(int fd, char *buf, int i) {
     perror("Can not write to stdout");
     exit(1);
   }
-}
+}*/
