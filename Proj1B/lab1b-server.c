1889025eb0ff098f43993e93afd9f3ee322f6553
Hongyang <l3848378@outlook.com>
2017-04-23 18:36:29 -0700

    Solved warnings and sanity check

--- Proj1B/lab1b-server.c
+++ Proj1B/lab1b-server.c
@@ -41,10 +41,11 @@ void pipe_handler(int signo) {
 
 int main(int argc, char *argv[])
 {
-  int sockfd, newsockfd, portno, clilen;
+  int sockfd, newsockfd, portno;
   char buffer[256];
   struct sockaddr_in serv_addr, cli_addr;
-  
+  socklen_t clilen;
+
   if (argc < 2) {
     fprintf(stderr,"ERROR, no port provided\n");
     exit(1);
