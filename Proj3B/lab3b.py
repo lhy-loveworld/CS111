e3f491f2e75060c7fd754b6c8a2e7127be672cfc
lhy-loveworld <l3848378@outlook.com>
2017-06-04 22:17:12 -0700

    sc

--- Proj3B/lab3b.py
+++ Proj3B/lab3b.py
@@ -63,7 +63,7 @@ def read_superblock(line):
 	INODE_BLOCKS = int(math.ceil(float(TOTAL_INODES) / (int(line[3]) / int(line[4]))))
 	first_nonrsv_inode = int(line[7])
 	blocks = [[-1 for col in range(3)]for row in range(TOTAL_BLOCKS+1)] 
-	inodes = [[-2 0 0] for _ in range(0, first_nonrsv_inode)] + [[-1 0 0] for _ in range(first_nonrsv_inode, TOTAL_INODES)]
+	inodes = [[-2, 0, 0] for _ in range(0, first_nonrsv_inode)] + [[-1, 0, 0] for _ in range(first_nonrsv_inode, TOTAL_INODES)]
 
 def read_group(line):
 	global first_nonrsv_block
@@ -207,7 +207,7 @@ def main():
 	for line in ID:
 		read_indirect(line)
 	scan_blocks()
-	print(TOTAL_BLOCKS, TOTAL_INODES, INODE_BLOCKS, len(blocks), len(inodes), first_nonrsv_block, first_nonrsv_inode)
+	#print(TOTAL_BLOCKS, TOTAL_INODES, INODE_BLOCKS, len(blocks), len(inodes), first_nonrsv_block, first_nonrsv_inode)
 	#
 	sys.exit(rc)
 
