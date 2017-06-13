ca382b1b8144b8693e62ebc9b8b3af1a569d1aac
Hongyang Li <hongyang@s-149-142-223-18.resnet.ucla.edu>
2017-06-13 16:58:08 -0700

    o

--- Proj4C/test.c
+++ Proj4C/test.c
@@ -49,13 +49,13 @@ int tcp_build(char* host, int port) {
 }
 
 SSL_CTX* InitCTX(void)
-{   SSL_METHOD *method;
+{   SSL_METHOD const *method;
     SSL_CTX *ctx;
 
     OpenSSL_add_all_algorithms();		/* Load cryptos, et.al. */
     SSL_load_error_strings();			/* Bring in and register error messages */
     SSL_library_init();
-    method = SSLv2_client_method();		/* Create new client-method instance */
+    method = SSLv23_client_method();		/* Create new client-method instance */
     ctx = SSL_CTX_new(method);			/* Create new context */
     if ( ctx == NULL )
     {
